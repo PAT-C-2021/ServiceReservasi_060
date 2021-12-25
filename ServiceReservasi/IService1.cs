6d655664afaf33bc48f641204fa997123f940514
KarmilatulHayyat <milahayyat2001@gmail.com>
2021-12-25 08:01:15 +0700

    Login&Register

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -25,6 +25,20 @@ namespace ServiceReservasi
         List<Pemesanan> Pemesanan();
 
 
+
+
+        [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+
         /*[OperationContract]
         string GetData(int value);
 
@@ -33,6 +47,18 @@ namespace ServiceReservasi
 
         // TODO: Add your service operations here
 
+    }
+
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 
     [DataContract]
